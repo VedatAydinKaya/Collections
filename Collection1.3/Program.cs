ce9234cd3d0bc4b776cd0d7f383275a9b9ef0357
vkayaonftech <vkaya@onftech.com>
2024-01-07 23:50:34 +0300

    vowelLetters  collections added

---
+++ Collection1.3/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+
+
+
+List<string> allVowelLetters = new List<string>();
+
+/* 
+ Klavyeden girilen cümle içerisindeki sesli 
+harfleri bir dizi içerisinde saklayan,
+ve dizinin elemanlarını sıralayan programı yazınız.
+ 
+ */
+//EUNOIA
+string[] vowelLetters = new string[] { "e", "u", "n","o","ı","a",
+    "E","U","N","O","I","A" };
+
+Console.Write("Please input the sentences:");
+
+string inputtedText=Console.ReadLine();
+
+if (!string.IsNullOrEmpty(inputtedText))
+{
+    char[] letters = inputtedText.Trim().ToCharArray();
+
+    string[] letterList = letters.Select(x => x.ToString()).ToArray();
+
+
+    foreach (string item in letterList)
+    {
+        if (vowelLetters.Contains(item))
+        {
+            allVowelLetters.Add(item);
+
+        }
+    }
+
+    Console.WriteLine("ALL VOWEL LETTERS");
+
+    foreach (var item in allVowelLetters)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+// Split the string into an array of characters
+
+
